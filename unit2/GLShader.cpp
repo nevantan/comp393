24ba77261279da2f428393836760801667a94c94
Nevan Tan <nevan@tanclan.ca>
2018-04-04 12:38:11 -0400

    Finish shader compilation function

--- unit2/GLShader.cpp
+++ unit2/GLShader.cpp
@@ -39,7 +39,7 @@ GLuint LoadShader(const char *vertex_path, const char *fragment_path) {
   GLint result = GL_FALSE;
   int logLength;
 
-  /* Compile vertex shader
+  // Compile vertex shader
   cout << "Compiling vertex shader." << endl;
   glShaderSource(vertShader, 1, &vertShaderSrc, NULL);
   glCompileShader(vertShader);
@@ -49,7 +49,36 @@ GLuint LoadShader(const char *vertex_path, const char *fragment_path) {
   glGetShaderiv(vertShader, GL_INFO_LOG_LENGTH, &logLength);
   vector<GLchar> vertShaderError((logLength > 1) ? logLength : 1);
   glGetShaderInfoLog(vertShader, logLength, NULL, &vertShaderError[0]);
-  cout << &vertShaderError[0] << endl;*/
+  cout << &vertShaderError[0] << endl;
 
-  return 0;
+  // Compile fragment shader
+  cout << "Compiling fragment shader." << endl;
+  glShaderSource(fragShader, 1, &fragShaderSrc, NULL);
+  glCompileShader(fragShader);
+
+  // Check fragment shader
+  glGetShaderiv(fragShader, GL_COMPILE_STATUS, &result);
+  glGetShaderiv(fragShader, GL_INFO_LOG_LENGTH, &logLength);
+  vector<GLchar> fragShaderError((logLength > 1) ? logLength : 1);
+  glGetShaderInfoLog(fragShader, logLength, NULL, &fragShaderError[0]);
+  cout << &fragShaderError[0] << endl;
+
+  // Link program
+  cout << "Linking program" << endl;
+  GLuint program = glCreateProgram();
+  glAttachShader(program, vertShader);
+  glAttachShader(program, fragShader);
+  glLinkProgram(program);
+
+  // Check program
+  glGetProgramiv(program, GL_LINK_STATUS, &result);
+  glGetProgramiv(program, GL_INFO_LOG_LENGTH, &logLength);
+  vector<char> programError((logLength > 1) ? logLength : 1);
+  glGetProgramInfoLog(program, logLength, NULL, &programError[0]);
+  cout << &programError[0] << endl;
+
+  glDeleteShader(vertShader);
+  glDeleteShader(fragShader);
+
+  return program;
 }
