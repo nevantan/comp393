d88ef1688a4fbb138916ecbe968223147a1cacf0
Nevan Tan <nevan@tanclan.ca>
2018-04-04 12:32:11 -0400

    Draw triangle

--- unit2/main.cpp
+++ unit2/main.cpp
@@ -3,17 +3,20 @@
 using namespace std;
 
 int main(int argc, char **argv) {
+  // Initialize GLFW
   if(!glfwInit()) {
     fprintf(stderr, "Failed to initialize GLFW\n");
     return -1;
   }
 
+  // Window Settings
   glfwWindowHint(GLFW_SAMPLES, 4);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+  // Create window and check that it is open
   GLFWwindow* window;
   window = glfwCreateWindow(1024, 768, "Tutorial 01", NULL, NULL);
   if(window == NULL) {
@@ -22,18 +25,47 @@ int main(int argc, char **argv) {
     return -1;
   }
   glfwMakeContextCurrent(window);
+
+  // Init GLEW
   glewExperimental = true;
   if(glewInit() != GLEW_OK) {
     fprintf(stderr, "Failed to initialize GLEW\n");
     return -1;
   }
 
+  // Setup Vertex Array Object
+  GLuint VertexArrayID;
+  glGenVertexArrays(1, &VertexArrayID);
+  glBindVertexArray(VertexArrayID);
+
+  // Load Shaders
+  GLuint program = LoadShader(argv[1], argv[2]);
+
+  // Draw Triangle
+  static const GLfloat g_vertex_buffer_data[] = {
+    -1.0f, -1.0f, 0.0f,
+    1.0f, -1.0f, 0.0f,
+    0.0f, 1.0f, 0.0f
+  };
+  GLuint vertexbuffer;
+  glGenBuffers(1, &vertexbuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+  // Setup input
   glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+  // Loop to keep window open and poll input
   do {
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDisableVertexAttribArray(0);
+
     glfwSwapBuffers(window);
     glfwPollEvents();
   } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
 
-  //LoadShader(argv[1], argv[2]);
   return 0;
 }
