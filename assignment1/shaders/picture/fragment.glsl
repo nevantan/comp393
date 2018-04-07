008bcfe8b01f2c0b704d5e5682c158eb56934b9f
Nevan Tan <nevan@tanclan.ca>
2018-04-07 16:52:05 -0400

    Add picture shaders and image

---
+++ assignment1/shaders/picture/fragment.glsl
@@ -0,0 +1,17 @@
+#version 450 core
+
+/*in vec2 UV;
+
+out vec3 color;
+
+uniform sampler2D Picture;
+
+void main() {
+  color = texture(Picture, UV).rgb;
+}*/
+
+out vec3 color;
+
+void main() {
+  color = vec3(1.0, 0.0, 0.0);
+}
