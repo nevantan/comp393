44781d545ec3fa731cd5718d1d990b41a85bc34c
Nevan Tan <nevan@tanclan.ca>
2018-04-26 15:01:06 -0400

    Add and modify shader class

---
+++ assignment5/shader.h
@@ -0,0 +1,27 @@
+#ifndef SHADER
+#define SHADER
+
+#include <GL/glew.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+class Shader {
+  GLuint vertShader;
+  GLuint fragShader;
+  GLuint progID;
+  GLint result;
+  int logLength;
+
+  std::string readFile(std::string filepath);
+  void compileShader(GLuint& shader, const char *shaderSrc);
+  void verifyShader(GLuint& shader);
+
+  public:
+    Shader(std::string vert, std::string frag);
+    ~Shader();
+    GLuint Program();
+};
+
+#endif
