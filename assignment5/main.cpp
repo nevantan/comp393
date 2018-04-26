a6e6be87e18627602c050d5d2e8257ece7a50e33
Nevan Tan <nevan@tanclan.ca>
2018-04-26 15:19:06 -0400

    Get test program working

--- assignment5/main.cpp
+++ assignment5/main.cpp
@@ -1,3 +1,6 @@
+#include "shader.h"
+#include "texture.h"
+
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -6,11 +9,44 @@
 using namespace std;
 using namespace glm;
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "lib/stb_image.h"
+
 GLFWwindow* window;
+GLuint VertexArrayID;
+
+GLuint vertexbuffer;
+Shader shader;
+
+void initScene() {
+  shader = Shader("shaders/shader.vert", "shaders/shader.frag");
+
+  static const GLfloat g_vertex_buffer_data[] = {
+     -1.0f, -1.0f, 0.0f,
+     1.0f, -1.0f, 0.0f,
+     0.0f,  1.0f, 0.0f,
+  };
+
+  glGenBuffers(1, &vertexbuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+}
+
+void drawScene() {
+  glUseProgram(shader.Program());
+
+  glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+  glDisableVertexAttribArray(0);
+}
 
 void render() {
   do {
     glClear(GL_COLOR_BUFFER_BIT);
+
+    drawScene();
 
     glfwSwapBuffers(window);
     glfwPollEvents();
@@ -46,8 +82,12 @@ int main() {
     return -1;
   }
 
+  glGenVertexArrays(1, &VertexArrayID);
+  glBindVertexArray(VertexArrayID);
+
   glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
+  initScene();
   render();
 
   return 0;
