be1260dff7a6c9891c9ed0bc54e563993e76bf0e
Nevan Tan <nevan@tanclan.ca>
2018-04-26 21:49:51 -0400

    Add camera and setup color buffer

--- final_project/main.cpp
+++ final_project/main.cpp
@@ -1,3 +1,4 @@
+#include "camera.h"
 #include "shader.h"
 
 #include <iostream>
@@ -13,10 +14,19 @@ GLFWwindow* window;
 GLuint VertexArrayID;
 
 GLuint vertexbuffer;
+GLuint colorbuffer;
 
+Camera camera;
 Shader basicShader;
 
 void initScene() {
+  camera = Camera(
+    (float)width / (float)height, // Aspect
+    vec3(4, 3, 3), // Position
+    vec3(0, 0, 0), // Target
+    vec3(0, 1, 0)  // Up
+  );
+
   basicShader = Shader("shaders/basic.vert", "shaders/basic.frag");
 
   static const GLfloat g_vertex_buffer_data[] = {
@@ -28,13 +38,30 @@ void initScene() {
   glGenBuffers(1, &vertexbuffer);
   glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
   glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+  static const GLfloat g_color_buffer_data[] = {
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f
+  };
+
+  glGenBuffers(1, &colorbuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
 }
 
 void drawScene() {
+  glUseProgram(basicShader.Program());
+  glUniformMatrix4fv(basicShader.Uniform("MVP"), 1, GL_FALSE, &camera.MVP()[0][0]);
+
   glEnableVertexAttribArray(0);
-    glUseProgram(basicShader.Program());
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+  glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
     glDrawArrays(GL_TRIANGLES, 0, 3);
   glDisableVertexAttribArray(0);
 }
