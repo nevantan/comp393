5143e09167c3457fca710ad1cb941cb3a4d63da4
Nevan Tan <nevan@tanclan.ca>
2018-04-26 22:07:19 -0400

    Add room geometry

--- final_project/main.cpp
+++ final_project/main.cpp
@@ -22,17 +22,44 @@ Shader basicShader;
 void initScene() {
   camera = Camera(
     (float)width / (float)height, // Aspect
-    vec3(4, 3, 3), // Position
-    vec3(0, 0, 0), // Target
+    vec3(-10, 12, 12), // Position
+    vec3(0, 4, 0), // Target
     vec3(0, 1, 0)  // Up
   );
 
   basicShader = Shader("shaders/basic.vert", "shaders/basic.frag");
 
   static const GLfloat g_vertex_buffer_data[] = {
-    -1.0f, -1.0f, 0.0f,
-    1.0f, -1.0f, 0.0f,
-    0.0f,  1.0f, 0.0f
+    -4.0f, 0.0f, -4.0f, // Back wall
+    -4.0f, 6.0f, -4.0f,
+    4.0f, 6.0f, -4.0f,
+    -4.0f, 0.0f, -4.0f,
+    4.0f, 6.0f, -4.0f,
+    4.0f, 0.0f, -4.0f,
+    4.0f, 0.0f, -4.0f, // Side wall
+    4.0f, 6.0f, -4.0f,
+    4.0f, 6.0f, 4.0f,
+    4.0f, 0.0f, -4.0f,
+    4.0f, 6.0f, 4.0f,
+    4.0f, 0.0f, 4.0f,
+    -4.0f, 0.0f, 4.0f, // Floor
+    -4.0f, 0.0f, -4.0f,
+    4.0f, 0.0f, -4.0f,
+    -4.0f, 0.0f, 4.0f,
+    4.0f, 0.0f, -4.0f,
+    4.0f, 0.0f, 4.0f,
+    -1.0f, 2.0f, -4.0f, // Window 1
+    -1.0f, 4.0f, -4.0f,
+    1.0f, 4.0f, -4.0f,
+    -1.0f, 2.0f, -4.0f,
+    1.0f, 4.0f, -4.0f,
+    1.0f, 2.0f, -4.0f,
+    4.0f, 2.0f, -1.0f, // Window 2
+    4.0f, 4.0f, -1.0f,
+    4.0f, 4.0f, 1.0f,
+    4.0f, 2.0f, -1.0f,
+    4.0f, 4.0f, 1.0f,
+    4.0f, 2.0f, 1.0f
   };
 
   glGenBuffers(1, &vertexbuffer);
@@ -40,9 +67,36 @@ void initScene() {
   glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
   static const GLfloat g_color_buffer_data[] = {
-    1.0f, 0.0f, 0.0f,
-    0.0f, 1.0f, 0.0f,
-    0.0f, 0.0f, 1.0f
+    0.5f, 0.5f, 0.5f, // Back wall
+    0.5f, 0.5f, 0.5f,
+    0.5f, 0.5f, 0.5f,
+    0.5f, 0.5f, 0.5f,
+    0.5f, 0.5f, 0.5f,
+    0.5f, 0.5f, 0.5f,
+    0.6f, 0.6f, 0.6f, // Side wall
+    0.6f, 0.6f, 0.6f,
+    0.6f, 0.6f, 0.6f,
+    0.6f, 0.6f, 0.6f,
+    0.6f, 0.6f, 0.6f,
+    0.6f, 0.6f, 0.6f,
+    0.4f, 0.4f, 0.4f, // Floor
+    0.4f, 0.4f, 0.4f,
+    0.4f, 0.4f, 0.4f,
+    0.4f, 0.4f, 0.4f,
+    0.4f, 0.4f, 0.4f,
+    0.4f, 0.4f, 0.4f,
+    0.9f, 0.9f, 0.9f, // Window 1
+    0.9f, 0.9f, 0.9f,
+    0.9f, 0.9f, 0.9f,
+    0.9f, 0.9f, 0.9f,
+    0.9f, 0.9f, 0.9f,
+    0.9f, 0.9f, 0.9f,
+    0.9f, 0.9f, 0.9f, // Window 2
+    0.9f, 0.9f, 0.9f,
+    0.9f, 0.9f, 0.9f,
+    0.9f, 0.9f, 0.9f,
+    0.9f, 0.9f, 0.9f,
+    0.9f, 0.9f, 0.9f
   };
 
   glGenBuffers(1, &colorbuffer);
@@ -62,7 +116,7 @@ void drawScene() {
     glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
 
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_TRIANGLES, 0, 30);
   glDisableVertexAttribArray(0);
 }
 
